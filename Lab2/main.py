3b56a995edc08effa24ab23d51be563a96eb4e2d
Ivanchenko Daniil <114954180+amicusdaniel@users.noreply.github.com>
2024-02-29 18:55:18 +0300

    Add files via upload

---
+++ Lab2/main.py
@@ -0,0 +1,9 @@
+from lab_python_oop import Circle
+from lab_python_oop import Rectangle
+from lab_python_oop import Square
+
+if __name__ == '__main__':
+    print(Rectangle.Rectangle("синий", 12, 13))
+    print(Circle.Circle("зеленый", 12))
+    print(Square.Square("красный", 12))
+
